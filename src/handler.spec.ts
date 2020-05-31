7c1a5d96931d336d0a9c2e7031bd9e721aebf6b4
Diego Herrera <608492+diegoh@users.noreply.github.com>
2020-05-31 14:50:31 +0100

    Fix broken tests

--- src/handler.spec.ts
+++ src/handler.spec.ts
@@ -4,6 +4,7 @@ jest.mock('./delay-request');
 
 import { createMockContext } from '@shopify/jest-koa-mocks';
 import { OK } from 'http-status-codes';
+import { Context } from 'koa';
 import { mocked } from 'ts-jest/utils';
 import { delayRequest } from './delay-request';
 import { handler } from './handler';
@@ -11,25 +12,25 @@ import { handler } from './handler';
 describe('src/handler', () => {
   const next = jest.fn();
   const MockedDelayRequest = mocked(delayRequest, true);
+  let ctx: Context;
+
+  beforeEach(() => {
+    ctx = createMockContext();
+  });
 
   it('sets the status to 200', async () => {
-    const ctx = createMockContext();
-
     await handler(ctx, next);
 
     expect(ctx.status).toBe(OK);
   });
 
   it('calls next', async () => {
-    const ctx = createMockContext();
-
     await handler(ctx, next);
 
     expect(next).toHaveBeenCalledTimes(1);
   });
 
   it('throws an error', async () => {
-    const ctx = createMockContext();
     ctx.request.query.error = true;
 
     try {
@@ -40,21 +41,10 @@ describe('src/handler', () => {
 
     expect.assertions(1);
   });
-  it('does not throw an error', async () => {
-    const ctx = createMockContext();
-    ctx.request.query.error = true;
 
-    try {
-      await handler(ctx, next);
-    } catch (error) {
-      expect(error).toBeTruthy();
-    }
-
-    expect.assertions(1);
-  });
   it('delays a request', async () => {
-    const ctx = createMockContext();
     const delay = 10000;
+    console.log(ctx.request.query);
     ctx.request.query.delay = delay;
 
     await handler(ctx, next);
